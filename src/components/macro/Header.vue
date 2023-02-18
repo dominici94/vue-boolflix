4e3a0370f49e05a4d29d8b5b3aa611bf85e5f7cb
dominici94 <mattiadominici94@gmail.com>
2023-02-18 18:28:19 +0100

    header fix style

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,15 +1,29 @@
 <template>
   <header class="header">
     <div class="header__left">
-      <img src="../../assets/img/boolflix.png" alt="logo boolflix" />
+      <img
+        src="../../assets/img/boolflix.png"
+        alt="logo boolflix"
+        class="header__logo"
+      />
 
       <nav class="header__nav">
         <ul class="header__nav--list">
-          <li class="header__nav--el">Home</li>
-          <li class="header__nav--el">TV Shows</li>
-          <li class="header__nav--el">Movies</li>
-          <li class="header__nav--el">Recently Added</li>
-          <li class="header__nav--el">My List</li>
+          <li class="header__nav--el">
+            <a href="#" class="header__nav--link active">Home</a>
+          </li>
+          <li class="header__nav--el">
+            <a href="#" class="header__nav--link"> TV Shows</a>
+          </li>
+          <li class="header__nav--el">
+            <a href="#" class="header__nav--link"> Movies</a>
+          </li>
+          <li class="header__nav--el">
+            <a href="#" class="header__nav--link"> Recently Added</a>
+          </li>
+          <li class="header__nav--el">
+            <a href="#" class="header__nav--link">My List</a>
+          </li>
         </ul>
       </nav>
     </div>
@@ -20,9 +34,13 @@
           <!-- <button>cerca film/telefilm</button> -->
           <input
             type="text"
-            placeholder="Cerca film o telefilm"
+            placeholder="Cerca..."
             v-model="dataShared.movie"
+            class="searchbar__input"
           />
+          <button class="searchbar__button">
+            <i class="fas fa-search"></i>
+          </button>
         </form>
       </div>
     </div>
@@ -76,24 +94,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/style/partials/variables.scss";
+
 .header {
-  height: 100px;
-  color: #000;
+  height: 10rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  // color: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
-  // background-color: black;
+  padding: 0 2rem;
+  background-image: linear-gradient(
+    to bottom,
+    rgba($color-black, 1),
+    rgba($color-black, 0.8)
+  );
 
   &__left,
   &__right {
     display: flex;
     align-items: center;
-    width: 50%;
-  }
-
-  &__right {
-    flex-direction: row-reverse;
   }
 
   &__nav--list {
@@ -102,19 +126,74 @@ export default {
   }
 
   &__nav--el {
-    margin-left: 15px;
+    margin-left: 1.5rem;
   }
 
-  img {
-    height: 70px;
+  &__nav--link:link,
+  &__nav--link:visited {
+    padding: 0.5rem 0;
+    display: inline-block;
+    text-decoration: none;
+    color: white;
+    font-size: 1.6rem;
+    transition: all 0.6s;
   }
 
-  .searchbar button,
-  input {
-    margin-right: 40px;
-    padding: 5px 20px;
-    border-radius: 20px;
-    background-color: red;
+  &__nav--link:hover,
+  &__nav--link:active,
+  .active {
+    color: $color-primary;
+    // border-bottom: 1px solid $color-primary-dark;
+    transform: translateY(-3px);
   }
+
+  &__logo {
+    height: 7rem;
+    margin-right: 2rem;
+  }
+
+  .searchbar {
+    color: white;
+
+    &__input {
+      background-color: $color-primary;
+      color: white;
+      border: none;
+      margin-right: 3rem;
+      // outline: $color-primary-dark;
+      // display: inline-block;
+      padding: 0.5rem 2rem;
+      border-radius: 20px;
+      &::placeholder {
+        color: white;
+      }
+    }
+
+    &__button {
+      background-color: $color-primary;
+      padding: 0.6rem 0.8rem;
+      border-radius: 7px;
+      cursor: pointer;
+      transition: all 0.3s;
+
+      &:hover {
+        background-color: $color-primary-dark;
+      }
+
+      & > * {
+        width: 1.5rem;
+        height: 1.2rem;
+        color: white;
+      }
+    }
+  }
+
+  // .searchbar button,
+  // input {
+  //   margin-right: 40px;
+  //   padding: 5px 20px;
+  //   border-radius: 20px;
+  //   background-color: $color-primary-dark;
+  // }
 }
 </style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -29,6 +29,10 @@ export default {
 
 <style>
 .main {
+  padding-top: 100px;
+  min-height: calc(100vh - 100px);
   background-image: url(https://static.standard.co.uk/2022/11/16/10/netflix-s.jpg?width=1200);
+  background-size: 100%;
+  /* background-repeat: no-repeat; */
 }
 </style>
